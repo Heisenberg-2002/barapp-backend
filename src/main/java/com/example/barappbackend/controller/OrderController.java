183e8f508c4add39dd625c0f84c54e6eff7748ac
Arthur-Gev2023 <arthur.gevaert@decathlon.com>
2024-06-03 15:45:35 +0200

    wip

--- src/main/java/com/example/barappbackend/controller/OrderController.java
+++ src/main/java/com/example/barappbackend/controller/OrderController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.barappbackend.model.Order;
 import com.example.barappbackend.service.OrderService;
 
+@CrossOrigin
 @Controller
 @RestController
 @RequestMapping("/v1/orders")
